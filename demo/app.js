cf958c363230e4891e864582211ffb5bf69887e5
jusjmkim <justinjmkim@gmail.com>
2016-09-10 15:02:03 -0400

    update demo app and powerpoin

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
 var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-  return res.send('hello world!');
+router.get('/', function (req, res) {
+  res.locals.title = 'JavaScript is awesome!';
+  res.locals.names = ['Justin', 'Julio', 'Sam'];
+  return res.render('index.html.ejs');
 });
+
+router.get('/hello', function (req, res) {
+  return res.send('hello Justin');
+});
+
+app.use(router);
 // Start listening for requests
 app.listen(port, function () {
   console.log('Listening on port ' + port);
